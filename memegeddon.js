6cf6109f2594fc386f783c4387cc733bbb38b196
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2019-01-28 09:33:00 +0000

    Uploader feature has been added

--- memegeddon.js
+++ memegeddon.js
@@ -1,12 +1,21 @@
-const {getMemesDb} = require('./lib/meme_data')
-const {downloadAllMemes} = require('./lib/files.js')
+const {getMemesDb} = require('./lib/meme_data'),
+    {downloadAllMemes} = require('./lib/files.js'),
+    {uploadAllFiles, publicAllFiles} = require('./lib/uploader'),
+    {delayPerPage} = require('./config');
 
 async function memesFunnel(memes){
-    memes = await downloadAllMemes(memes)
+    try {
+        memes = await downloadAllMemes(memes);
+        await uploadAllFiles(memes);
+        memes = await publicAllFiles(memes);
+    } catch (err) {
+        console.log(`[Memegeddon][ERROR][SYSTEM FAILURE] MemeFunnel has broken.`);
+        console.error(err);
+    }
 }
 
 
-getMemesDb("http://alpha-meme-maker.herokuapp.com", 30000)
+getMemesDb("http://alpha-meme-maker.herokuapp.com", delayPerPage)
     .on("data", memesFunnel)
     .on("end",  data => console.log("-- TOTAL MEMES:", data.length))
     .on("error", console.log);
