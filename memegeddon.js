8088595d541a1041bcdf9d6d0bffb35cc112b7a1
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2019-01-28 07:56:43 +0000

    Say Hello to memesFunnel! :muscle:

--- memegeddon.js
+++ memegeddon.js
@@ -1,6 +1,12 @@
 const {getMemesDb} = require('./lib/meme_data')
+const {downloadAllMemes} = require('./lib/files.js')
 
-getMemesDb("http://alpha-meme-maker.herokuapp.com", 1000)
-    .on("data", data => console.log("-- TOTAL Memes in Page:", data.length))
+async function memesFunnel(memes){
+    memes = await downloadAllMemes(memes)
+}
+
+
+getMemesDb("http://alpha-meme-maker.herokuapp.com", 30000)
+    .on("data", memesFunnel)
     .on("end",  data => console.log("-- TOTAL MEMES:", data.length))
     .on("error", console.log);
