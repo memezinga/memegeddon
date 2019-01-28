a016822533fcb045fce333b4fd3e76618badc9b2
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2019-01-28 10:47:08 +0000

    firebase uploader added as a feature

--- memegeddon.js
+++ memegeddon.js
@@ -1,13 +1,22 @@
 const {getMemesDb} = require('./lib/digester'),
     {downloadAllMemes} = require('./lib/downloader.js'),
     {uploadAllFiles, publicAllFiles} = require('./lib/uploader'),
-    {delayPerPage} = require('./config');
+    {saveFile, timestamp} = require('./lib/utils'),
+    {delayPerPage, paths} = require('./config'),
+    {saveMemes} = require('./lib/database');
+
+async function stopEngine(originalMemes){
+    console.log("-- TOTAL MEMES:", originalMemes.length);
+    process.exit();
+}
 
 async function memesFunnel(memes){
     try {
         memes = await downloadAllMemes(memes);
         await uploadAllFiles(memes);
         memes = await publicAllFiles(memes);
+        await saveFile(`${paths.data}/${timestamp()}.json`, JSON.stringify(memes), "utf8");
+        await saveMemes(memes);
     } catch (err) {
         console.log(`[Memegeddon][ERROR][SYSTEM FAILURE] MemeFunnel has broken.`);
         console.error(err);
@@ -17,5 +26,5 @@ async function memesFunnel(memes){
 
 getMemesDb("http://alpha-meme-maker.herokuapp.com", delayPerPage)
     .on("data", memesFunnel)
-    .on("end",  data => console.log("-- TOTAL MEMES:", data.length))
+    .on("end",  stopEngine)
     .on("error", console.log);
